927593d4b0040b0336b7476234249f2bc0f4d435
Oren Magid <autoimpedicis@gmail.com>
2018-03-13 11:19:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = []
   for (var i = 0; musicians[i]; i++) {
-    array.push(`"${musicians} plays ${instruments}"`)
+    array.push(`"${musicians[i]} plays ${instruments[i]}"`)
 }
 return array
 }
