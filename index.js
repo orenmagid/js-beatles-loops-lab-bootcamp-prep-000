0a0fa4bb5213e1fa87a3bc0691eeffeb1a1dc28d
Oren Magid <autoimpedicis@gmail.com>
2018-03-13 11:35:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ function theBeatlesPlay(musicians, instruments) {
   var array = []
   for (var i = 0; musicians[i]; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`)
-}
+  }
 return array
 }
 
 function johnLennonFacts(facts){
   while (facts[i]) {
   facts[i] += "!!!"
-}
+  }
 return facts
+}
+
+function iLoveTheBeatles(number) {
+  var array = []
+    do {
+      array.push("I love the Beatles!")
+        number++
+    } while (number < 15)
+    return array
 }
